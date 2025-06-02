c18ec4a6da9d49f54aa2bc2a623b13d1005438d7
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 12:04:17 +0600

    Update demo_log_parse.rb

--- demo_log_parse.rb
+++ demo_log_parse.rb
@@ -17,7 +17,7 @@ File.foreach(file_name) do |line|
   #puts "#{counter}"
   p line.split(']')[-1]
   p par_info["PAR_ID"][counter] = line.split('=')[1].split(',')[0]
-  p par_info["THERAP_ID"][counter] = line.split('=')[2].split(',')[0]
+  p par_info["r_ID"][counter] = line.split('=')[2].split(',')[0]
   p par_info["REGION"][counter] = line.split('=')[3].split(',')[0]
   p par_info["PROVIDER"][counter] = line.split('=')[4].split(',')[0]
   p par_info["DDPM"][counter] = line.split('=')[5].split(',')[0]
@@ -37,10 +37,10 @@ counter1= 0
 
 #=begin
 CSV.open("demo_log_total.csv", "wb") do |csv|
-  csv << ["PAR ID","Therap ID","Region", "Provider", "DDPM", "OLD_HCBS_SCREENABLE_FLAG", "NEW_HCBS_SCREENABLE_FLAG"]
+  csv << ["PAR ID","r ID","Region", "Provider", "DDPM", "OLD_HCBS_SCREENABLE_FLAG", "NEW_HCBS_SCREENABLE_FLAG"]
 
   for counter1 in 0...counter
-     csv << ["#{par_info["PAR_ID"][counter1]}","#{par_info["THERAP_ID"][counter1]}", par_info["REGION"][counter1], par_info["PROVIDER"][counter1], par_info["DDPM"][counter1], par_info["OLD_HCBS_SCREENABLE_FLAG"][counter1], par_info["NEW_HCBS_SCREENABLE_FLAG"][counter1] ]
+     csv << ["#{par_info["PAR_ID"][counter1]}","#{par_info["r_ID"][counter1]}", par_info["REGION"][counter1], par_info["PROVIDER"][counter1], par_info["DDPM"][counter1], par_info["OLD_HCBS_SCREENABLE_FLAG"][counter1], par_info["NEW_HCBS_SCREENABLE_FLAG"][counter1] ]
   end
   #}
 end
