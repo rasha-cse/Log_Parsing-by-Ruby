06748a5d38d087e8f1b8bf52b30c33b1adbd6da6
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 12:11:12 +0600

    Update sis_tracking_num_mismatch.rb

--- sis_tracking_num_mismatch.rb
+++ sis_tracking_num_mismatch.rb
@@ -2,16 +2,16 @@ require 'csv'
 require 'time'
 
 #file_name= "demo_log.txt"
-#file_name= "sis_mismatch_all_86_messages.txt"
-#file_name2 = "therap-xfer01-fx-1472533209565.xml"
-file_name2 = "therap_apr_1_2017.xml"
+#file_name= "r_mismatch_all_86_messages.txt"
+#file_name2 = "r-xfer01-fx-1472533209565.xml"
+file_name2 = "r_apr_1_2017.xml"
 #par_info = Hash.new{|hsh,key| hsh[key] = []}
 
 counter = 1
-p "SIS Tracking Numbers: "
+p "r Tracking Numbers: "
 File.foreach(file_name2) do |line|
 
-  if /sis_track_num/.match(line) #|| /SIS id:/.match(line)     #SIS tracking number:, SIS id: #/SIS tracking number:/ <- file_name
+  if /r_track_num/.match(line) #|| /r id:/.match(line)     #r tracking number:, r id: #/r tracking number:/ <- file_name
     puts "#{counter}. "+ line.split('>')[1].split('<')[0]
     counter += 1
   end
