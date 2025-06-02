22c1a3d81d83499ba6e12a2150673e48e0dbe458
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 12:07:43 +0600

    Update log_parse_presentation.rb

--- log_parse_presentation.rb
+++ log_parse_presentation.rb
@@ -1,11 +1,11 @@
 puts "Enter the desired string:"
-file_name= "state-nd.2016-03-26.log" #"therap.log" #"test.txt"
+file_name= "-.2016-03-26.log" #"therap.log" #"test.txt"
 counter = 1
 counter2 = 1
 txt = ""
 File.foreach(file_name) do |line|                                        # reading from file
 
-  if /grails.app.controllers.net.therap.states.par.ParScoreMigrationController:-1/.match(line) && /INFO  -/.match(line) && /SUCCESS/.match(line) && /PAR_ID=/.match(line)  #&&/2015-02-03	11:19:05/.match(line)                    # finding matches
+  if /grails.app.controllers.net.therap.states.par.ParScoreMigrationController:-1/.match(line) && /INFO  -/.match(line) && /SUCCESS/.match(line) && /rid=/.match(line)  #&&/2015-02-03	11:19:05/.match(line)                    # finding matches
 
     puts "#{line}"
     #matched_line += line
