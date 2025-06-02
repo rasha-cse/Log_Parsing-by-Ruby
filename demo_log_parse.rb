2c19d892109a3044557824054164956e61effca6
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 12:05:46 +0600

    Update demo_log_parse.rb

--- demo_log_parse.rb
+++ demo_log_parse.rb
@@ -5,7 +5,7 @@ require 'time'
 file_name= "log_23_28_03_16"
 
 #login_session_id = Hash.new( 0 )
-par_info = Hash.new{|hsh,key| hsh[key] = []}
+r_info = Hash.new{|hsh,key| hsh[key] = []}
 #session_end_time = Hash.new( 0 )
 
 counter = 0
@@ -16,13 +16,13 @@ File.foreach(file_name) do |line|
   if /SUCCESS/.match(line)
   #puts "#{counter}"
   p line.split(']')[-1]
-  p par_info["PAR_ID"][counter] = line.split('=')[1].split(',')[0]
-  p par_info["r_ID"][counter] = line.split('=')[2].split(',')[0]
-  p par_info["REGION"][counter] = line.split('=')[3].split(',')[0]
-  p par_info["PROVIDER"][counter] = line.split('=')[4].split(',')[0]
-  p par_info["DDPM"][counter] = line.split('=')[5].split(',')[0]
-  p par_info["OLD_HCBS_SCREENABLE_FLAG"][counter] = line.split('=')[6].split(',')[0]
-  p par_info["NEW_HCBS_SCREENABLE_FLAG"][counter] = line.split('=')[7].split(',')[0].chomp
+  p r_info["r_ID"][counter] = line.split('=')[1].split(',')[0]
+  p r_info["r_ID"][counter] = line.split('=')[2].split(',')[0]
+  p r_info["REGION"][counter] = line.split('=')[3].split(',')[0]
+  p r_info["PROVIDER"][counter] = line.split('=')[4].split(',')[0]
+  p r_info["DDPM"][counter] = line.split('=')[5].split(',')[0]
+  p r_info["OLD_HCBS_SCREENABLE_FLAG"][counter] = line.split('=')[6].split(',')[0]
+  p r_info["NEW_HCBS_SCREENABLE_FLAG"][counter] = line.split('=')[7].split(',')[0].chomp
 
   #p line.split(' ')[12].split('=')[0]
 
@@ -32,15 +32,15 @@ File.foreach(file_name) do |line|
   end
 end
 
-#p par_info
+#p r_info
 counter1= 0
 
 #=begin
 CSV.open("demo_log_total.csv", "wb") do |csv|
-  csv << ["PAR ID","r ID","Region", "Provider", "DDPM", "OLD_HCBS_SCREENABLE_FLAG", "NEW_HCBS_SCREENABLE_FLAG"]
+  csv << ["r ID","r ID","Region", "Provider", "DDPM", "OLD_HCBS_SCREENABLE_FLAG", "NEW_HCBS_SCREENABLE_FLAG"]
 
   for counter1 in 0...counter
-     csv << ["#{par_info["PAR_ID"][counter1]}","#{par_info["r_ID"][counter1]}", par_info["REGION"][counter1], par_info["PROVIDER"][counter1], par_info["DDPM"][counter1], par_info["OLD_HCBS_SCREENABLE_FLAG"][counter1], par_info["NEW_HCBS_SCREENABLE_FLAG"][counter1] ]
+     csv << ["#{r_info["r_ID"][counter1]}","#{r_info["r_ID"][counter1]}", r_info["REGION"][counter1], r_info["PROVIDER"][counter1], r_info["DDPM"][counter1], r_info["OLD_HCBS_SCREENABLE_FLAG"][counter1], r_info["NEW_HCBS_SCREENABLE_FLAG"][counter1] ]
   end
   #}
 end
